4f7a4b062cc9828e51792cd053cad7a20c6af5d9
Carl Kittelberger <icedream@icedream.pw>
2022-03-28 18:00:01 +0200

    Do not rectify slave zones.

--- pdns/secallzones.sh
+++ pdns/secallzones.sh
@@ -3,28 +3,29 @@
 echo "[`date +"%T"`] Secallzones starting... "
 ZONES=`pdnsutil list-all-zones | grep -v "All zonecount"`
 while read -r d; do
-  pdnsutil show-zone $d | grep presigned >/dev/null 2>&1
-  if [ $? -eq 0 ] ; then
-    echo "Securing $d..."
-    pdnsutil unset-presigned $d
-    pdnsutil secure-zone $d
-    pdnsutil rectify-zone $d
-    fixdsrrs.sh -d $d
-  else
-    pdnsutil show-zone $d | grep "not actively secured" >/dev/null 2>&1
-    if [ $? -eq 0 ] ; then
-      echo "Securing $d..."
-      pdnsutil secure-zone $d
-      pdnsutil rectify-zone $d
-      fixdsrrs.sh -d $d
-    fi
+  domaininfo="$(pdnsutil show-zone "$d")"
+
+  printf "%s: " "$d"
+
+  # do not sign or attempt to resign slave zones
+  if [[ "$domaininfo" =~ "Slave zone" ]]; then
+    echo "slave zone, skipping"
+    continue
+  fi
+ 
+  if [[ "$domaininfo" =~ "Zone is presigned" ]]; then
+    # not a slave zone, remove presigned flag
+    pdnsutil unset-presigned "$d"
+    printf "presigned flag removed, "
+  elif ! [[ "$domaininfo" =~ "not actively secured" ]]; then
+    # this is already secured, skip
+    echo "already secured, skipping."
+    continue
   fi
 
+  pdnsutil secure-zone "$d"
+  pdnsutil rectify-zone "$d"
+  fixdsrrs.sh -d "$d"
+  echo "secured."
 done <<< "$ZONES"
-
-echo -n "[`date +"%T"`] Rectifying all zones..."
-pdnsutil rectify-all-zones &> /dev/null && echo " OK."
-
 echo "[`date +"%T"`] Secallzones finished."
-
-
